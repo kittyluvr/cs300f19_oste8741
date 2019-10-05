7f4c2effa4225e663f4ad7eabc9af2fe2dd61afb
frisk oster <oste8741@pacificu.edu>
2019-10-05 00:08:28 -0700

    Implemented all pqueue functions, wrote tester

--- GenericDynamicPriorityQ/src/pqueue.c
+++ GenericDynamicPriorityQ/src/pqueue.c
@@ -11,25 +11,61 @@
 #include "../../GenericDynamicList/include/list.h"
 #include "../include/pqueue.h"
 
+char gszPQErrors[NUMBER_OF_LIST_ERRORS][MAX_ERROR_LIST_CHARS];
+
+/**************************************************************************
+ Function: 	 	processError
+
+ Description: Process the error code passed to this routine
+
+ Parameters:	pszFunctionName - function causing the error
+ 	 	 	 	 	 	 	errorCode 	    - identifies the stack error
+
+ Returned:	 	None
+ *************************************************************************/
+static void processError (const char *pszFunctionName, int errorCode){
+	printf ("Function: %s %s \n ", pszFunctionName,
+					 gszPQErrors[errorCode]);
+	exit (EXIT_FAILURE);
+} //This is just copied from stk.c
+
 //*************************************************************************
 //										Allocation and Deallocation
 //*************************************************************************
 extern void pqueueCreate (PriorityQueuePtr psQueue
 													/*, 	cmpFunction cmpFunct */){
-
+	if(psQueue == NULL){
+		processError("pqueueCreate", ERROR_NO_PQ_CREATE);
+	}
+	lstCreate(&psQueue->sTheList);
+	return;
 }
 // results: If PQ can be created, then PQ exists and is empty
 //					otherwise, ERROR_NO_PQ_CREATE
 
 
 extern void pqueueTerminate (PriorityQueuePtr psQueue){
+	if(psQueue == NULL){
+		processError("pqueueTerminate", ERROR_NO_PQ_TERMINATE);
+	}
 
+	PriorityQueueElementPtr psTemp;
+
+	while(!lstIsEmpty(&psQueue->sTheList)){
+		lstFirst(&psQueue->sTheList);
+		lstDeleteCurrent(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+		free(psTemp->pData);
+	}
+
+	lstTerminate(&psQueue->sTheList);
+	return;
 }
 // results: If PQ can be terminated, then PQ no longer exists and is empty
 //				   otherwise, ERROR_NO_PQ_TERMINATE
 
 extern void pqueueLoadErrorMessages (){
-
+	LOAD_PQ_ERRORS
+	lstLoadErrorMessages();
 }
 // results:	Loads the error message strings for the error handler to use
 //					No error conditions
@@ -39,13 +75,13 @@ extern void pqueueLoadErrorMessages (){
 //*************************************************************************
 
 extern int pqueueSize (const PriorityQueuePtr psQueue){
-	return 0;
+	return lstSize(&psQueue->sTheList);
 }
 // results: Returns the number of elements in the PQ
 // 					error code priority: ERROR_INVALID_PQ if PQ is NULL
 
 extern bool pqueueIsEmpty (const PriorityQueuePtr psQueue){
-	return true;
+	return lstIsEmpty(&psQueue->sTheList);
 }
 // results: If PQ is empty, return true; otherwise, return false
 // 					error code priority: ERROR_INVALID_PQ
@@ -57,7 +93,51 @@ extern bool pqueueIsEmpty (const PriorityQueuePtr psQueue){
 
 extern void pqueueEnqueue (PriorityQueuePtr psQueue, const void *pBuffer,
 										int size, int priority){
+	if(psQueue == NULL){
+		processError("pqueueEnqueue", ERROR_INVALID_PQ);
+	}
+	if(pBuffer == NULL){
+		processError("pqueueEnqueue", ERROR_NULL_PQ_PTR);
+	}
 
+	PriorityQueueElementPtr psTemp = NULL;
+	PriorityQueueElementPtr psNew = malloc(sizeof(PriorityQueueElement));
+	psNew->priority = priority;
+	psNew->pData = malloc(size);
+	memcopy(psNew->pData, pBuffer, size);
+
+	if(lstIsEmpty(&psQueue->sTheList)){
+		lstInsertAfter(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+	}
+
+	else{
+		//Check if lowest priority
+		lstLast(&psQueue->sTheList);
+		lstPeek(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+		if(psNew->priority >= psTemp->priority){
+			lstInsertAfter(&psQueue->sTheList, psNew, sizeof(PriorityQueueElement));
+		}
+
+		else{
+			//Check if highest priority
+			lstFirst(&psQueue->sTheList);
+			lstPeek(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+			if(psNew->priority < psTemp->priority){
+				lstInsertBefore(&psQueue->sTheList, psNew, sizeof(PriorityQueueElement));
+			}
+
+			else{
+				//Otherwise walk until next is lower priority
+				lstPeekNext(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+				while(psNew->priority >= psTemp->priority){
+					lstNext(&psQueue->sTheList);
+					lstPeekNext(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+				}
+				lstInsertAfter(&psQueue->sTheList, psNew, sizeof(PriorityQueueElement));
+			}
+		}
+	}
+	return;
 }
 // requires: psQueue is not full
 // results: Insert the element into the priority queue based on the
@@ -68,7 +148,24 @@ extern void pqueueEnqueue (PriorityQueuePtr psQueue, const void *pBuffer,
 
 extern void *pqueueDequeue (PriorityQueuePtr psQueue, void *pBuffer,
 														int size, int  *pPriority){
-	return NULL;
+	if(psQueue == NULL){
+		processError("pqueueDequeue", ERROR_INVALID_PQ);
+	}
+	if(pBuffer == NULL){
+		processError("pqueueDequeue", ERROR_NULL_PQ_PTR);
+	}
+	if(pqueueIsEmpty(psQueue)){
+		processError("pqueueDequeue", ERROR_EMPTY_PQ);
+	}
+
+	PriorityQueueElement psTemp = NULL;
+
+	lstFirst(&psQueue->sTheList);
+	lstDeleteCurrent(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+	memcpy(pBuffer, psTemp->pData, size);
+	*pPriority = psTemp->priority;
+
+	return pBuffer;
 }
 
 // requires: psQueue is not empty
@@ -82,7 +179,22 @@ extern void *pqueueDequeue (PriorityQueuePtr psQueue, void *pBuffer,
 
 extern void *pqueuePeek (PriorityQueuePtr psQueue, void *pBuffer, int size,
 								 int *priority){
-	return NULL;
+	if(psQueue == NULL){
+		processError("pqueuePeek", ERROR_INVALID_PQ);
+	}
+	if(pBuffer == NULL){
+		processError("pqueuePeek", ERROR_NULL_PQ_PTR);
+	}
+	if(pqueueIsEmpty(psQueue)){
+		processError("pqueuePeek", ERROR_EMPTY_PQ);
+	}
+
+	PriorityQueueElementPtr psTemp = NULL;
+	lstPeek(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+	*priority = psTemp->priority;
+	memcpy(pBuffer, psTemp->pData, size);
+
+	return pBuffer;
 }
 // requires: psQueue is not empty
 // results: The priority and value of the first element is returned through
@@ -93,7 +205,26 @@ extern void *pqueuePeek (PriorityQueuePtr psQueue, void *pBuffer, int size,
 
 extern void pqueueChangePriority (PriorityQueuePtr psQueue,
 																	int change){
+	if(psQueue == NULL){
+		processError("pqueueChangePriority", ERROR_INVALID_PQ);
+	}
 
+	PriorityQueueElementPtr psTemp = NULL;
+
+	//if PQ is empty then do nothing
+	if(pqueueIsEmpty(psQueue)){
+		lstFirst(&psQueue->sTheList);
+		lstPeek(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+		psTemp->priority += change;
+		lstUpdateCurrent(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+		while(lstHasNext(&psQueue->sTheList)){
+			lstNext(&psQueue->sTheList);
+			lstPeek(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+			psTemp->priority += change;
+			lstUpdateCurrent(&psQueue->sTheList, psTemp, sizeof(PriorityQueueElement));
+		}
+	}
+	return;
 }
 // results: The priority of all elements is increased by the amount in
 //					change error code priority: ERROR_INVALID_PQ
--- GenericDynamicPriorityQ/src/pqueuetester.c
+++ GenericDynamicPriorityQ/src/pqueuetester.c
@@ -6,6 +6,132 @@
  */
 #include "../include/pqueue.h"
 
+/****************************************************************************
+ Function: 	 	success
+
+ Description: print a success message
+
+ Parameters:	pszStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void success(char *pszStr) {
+	printf("SUCCESS: %s\n", pszStr);
+}
+
+/****************************************************************************
+ Function: 	 	failure
+
+ Description: print a failure message
+
+ Parameters:	pszStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void failure(char *pszStr) {
+	printf("FAILURE: %s\n", pszStr);
+}
+
+/****************************************************************************
+ Function: 	 	assert
+
+ Description: if the expression is true, assert success; otherwise, assert
+ failure
+
+ Parameters:	szStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void assert(bool bExpression, char *pTrue, char *pFalse) {
+	if (bExpression) {
+		success(pTrue);
+	} else {
+		failure(pFalse);
+	}
+}
+
 int main(){
+	const int STRING_MAX = 256;
+	const int TOP_PRIORITY = 0;
+	const int LOOP_START = 0;
+	const int TEN = 10;
+	const int TEST_SIZE = TEN * TEN;
+	const int TEST_SIZE_2 = 20;
+	const int PRIORITY_INCREASE = 3;
+
+	char szMsg[STRING_MAX];
+
+	bool loopCheck = true;
+
+	int testInt = 23;
+	int bufferInt = 0;
+	int bufferPriority = 1;
+	int i = 0;
+	int fancyTestLocation = 0;
+
+	pqueueLoadErrorMessages();
+
+	PriorityQueue sQueue;
+	pqueueCreate(&sQueue);
+	assert(pqueueIsEmpty(&sQueue)&&pqueueSize(&sQueue), "Queue is empty", "Why does queue have data or size does not match empty?");
+
+	pqueueEnqueue(&sQueue, &testInt, sizeof(int), TOP_PRIORITY);
+	assert(pqueueSize(&sQueue) == 1, "Queue has an element", "Enqueue error");
+	pqueuePeek(&sQueue, &bufferInt, sizeof(int), &bufferPriority);
+	assert(bufferInt == testInt, "Peek correct", "Peek error");
+	assert(bufferPriority == TOP_PRIORITY, "Priority check correct", "Priority changed somehow?");
+	bufferInt = 0;
+	bufferPriority = 1;
+	pqueueDequeue(&sQueue, &bufferInt, sizeof(int), &bufferPriority);
+	assert(bufferInt == testInt, "Dequeue correct", "Dequeue error");
+	assert(bufferPriority == TOP_PRIORITY, "Priority check correct", "Priority changed somehow?");
+	assert(pqueueIsEmpty(&sQueue), "Queue is empty again", "Queue element still there?");
+
+	for(i = LOOP_START; i < TEST_SIZE; i++){
+		pqueueEnqueue(&sQueue, &i, sizeof(int), i % 10);
+	}
+	assert(pqueueSize(&sQueue) == TEST_SIZE - LOOP_START, "Queue is the correct size", "Weird number of queue elements");
+	pqueueChangePriority(&sQueue, PRIORITY_INCREASE);
+	for(i = LOOP_START; i < TEST_SIZE_2; i++){
+		pqueueEnqueue(&sQueue, &i, sizeof(int), i/10);
+	}
+	assert(pqueueSize(&sQueue) == (TEST_SIZE - LOOP_START + TEST_SIZE_2 - LOOP_START), "Queue is the correct size", "Weird number of queue elements");
+	pqueuePeek(&sQueue, &bufferInt, sizeof(int), &bufferPriority);
+	assert(bufferInt = LOOP_START, "Loop order seems correct, will confirm", "unexpected first number");
+
+	for(i = LOOP_START; loopCheck && i < TEST_SIZE_2; i++){
+		pqueueDequeue(&sQueue, &bufferInt, sizeof(int), &bufferPriority);
+		if(i != bufferInt || i/10 != bufferPriority){
+			loopCheck = false;
+			sprintf(szMsg, "Error in second set added. Value is %d, expected %d, priority is %d, expected %d", bufferInt, i, bufferPriority, i/10);
+		}
+		bufferInt = 0;
+		bufferPriority = 0;
+	}
+	assert(loopCheck, "Change Priority and second set adding at start successful", szMsg);
+
+	loopCheck = true;
+	for(fancyTestLocation = LOOP_START; loopCheck && fancyTestLocation < TEN; fancyTestLocation++){
+		for(i = LOOP_START; loopCheck && i < TEN; i++){
+			pqueueDequeue(&sQueue, &bufferInt, sizeof(int), &bufferPriority);
+			if(bufferInt != i * TEN + fancyTestLocation || bufferPriority != PRIORITY_INCREASE){
+				loopCheck = false;
+				sprintf(szMsg, "Value is %d, expected %d, priority is %d, expected %d", bufferInt, i * TEN + fancyTestLocation, bufferPriority, fancyTestLocation);
+			}
+			bufferInt = 0;
+			bufferPriority = 0;
+		}
+		assert(loopCheck, "this check successful", szMsg);
+	}
+
+	pqueueTerminate(&sQueue);
+	assert(pqueueIsEmpty(&sQueue), "Terminate Successful", "Terminate Error");
+
+	for(i = LOOP_START; i < TEN; i++){
+		pqueueEnqueue(&sQueue, i, sizeof(int), i);
+	}
+	pqueueTerminate(&sQueue);
+	assert(pqueueIsEmpty(&sQueue), "Terminate Successful", "Terminate Error");
+
 	return EXIT_SUCCESS;
 }
