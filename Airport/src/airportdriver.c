694ce44d1e1aa48ed3045c29b236bb12ff929ef9
frisk oster <oste8741@pacificu.edu>
2019-10-28 11:06:07 -0700

    Started airportdriver

--- Airport/src/airportdriver.c
+++ Airport/src/airportdriver.c
@@ -1,3 +1,28 @@
-int main(){
+#include "../include/airport.h"
+#include <stdio.h>
+
+void runSimulation(AirportPtr psAirport, FILE* inFile);
+void printStats(AirportPtr psAirport);
+
+int main(int argc, char* argv[]){
+	FILE* inFile = NULL;
+	Airport sAirport;
+
+	//Check received enough for a file name.
+	if(argc == 2){
+		//Try opening file
+		inFile = fopen(argv[1], "r");
+		if(inFile != NULL){
+			runSimulation(&sAirport, inFile);
+			printStats(&sAirport);
+		}
+		else{
+			printf("Could not open file");
+		}
+		fclose(inFile);
+	}
+	else{
+		printf("Unexpected number of arguments");
+	}
 	return 0;
 }
